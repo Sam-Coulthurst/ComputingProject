ba030a791896ef0ca658de8f096473e4eabda8f3
Sam Coulthurst <pccx32@durham.ac.uk>
2025-10-15 20:03:20 +0100

    Milestone Problem solved ?

--- Restricted_3_body_problem.py
+++ Restricted_3_body_problem.py
@@ -8,12 +8,11 @@ d = 384400000 # m
 time_step = 10 #s
 
 T = 2*np.pi*np.sqrt(d**3/(G*(m_e + m_m)))
-max_time = T
+max_time = 2*T
 time = np.arange(0, max_time, time_step) #s
 n_steps = len(time)
 
-r0 = np.array([ 4.672203365e6, 6.571000e6])  # m
-v0 = np.array([ 10988.378, 12.452740 ]) # m/s
+
 
 def pos_earth_moon(t,circular=True):
     """
@@ -30,15 +29,15 @@ def pos_earth_moon(t,circular=True):
         # Angular velocity
         omega = 2*np.pi/T # rad/s
 
-        # Earth position (assuming circular orbit around barycenter)
+        # Earth 
         r_e = d*m_m/(m_e + m_m) # m
-        x_e = r_e * np.cos(omega*t) # x position of Earth, m
-        y_e = r_e * np.sin(omega*t) # y position of Earth, m
+        x_e = r_e * np.cos(omega*t) #x, m
+        y_e = r_e * np.sin(omega*t) #y, m
 
-        # Moon position (assuming circular orbit around barycenter)
+        # Moon 
         r_m = d*m_e/(m_e + m_m) # m
-        x_m = r_m * np.cos(omega*t + np.pi) # x position of Moon, m
-        y_m = r_m * np.sin(omega*t + np.pi) # y position of Moon, m
+        x_m = r_m * np.cos(omega*t + np.pi) #x, m
+        y_m = r_m * np.sin(omega*t + np.pi) #y, m
 
         pos_earth = np.array([x_e, y_e])
         pos_moon = np.array([x_m, y_m])
@@ -46,8 +45,6 @@ def pos_earth_moon(t,circular=True):
     else:
         raise(NotImplementedError("Elliptical orbits not yet implemented"))
     
-
-
 def acceleration(r, t):
     '''
     This function calculates the acceleration on a rocket at position (x,y) at time t
@@ -55,8 +52,8 @@ def acceleration(r, t):
     pos_earth, pos_moon = pos_earth_moon(t)
     x = r[0]
     y = r[1]
+    
     # Distance vectors
-
     r_earth = np.array([x - pos_earth[0], y - pos_earth[1]])
     r_moon = np.array([x - pos_moon[0], y - pos_moon[1]])
 
@@ -64,15 +61,15 @@ def acceleration(r, t):
     r_earth_mag = np.linalg.norm(r_earth)
     r_moon_mag = np.linalg.norm(r_moon)
 
-    # Gravitational acceleration
+    # Calculate acclerations
     a_earth = -G * m_e * r_earth / r_earth_mag**3
     a_moon = -G * m_m * r_moon / r_moon_mag**3
 
     return a_earth + a_moon
 
 
-
 def evolve(r_rocket,v_rocket, n_steps, method = 'RK4'):
+    
     # Preallocate
     r = np.zeros((2, n_steps))
     v = np.zeros((2, n_steps))
@@ -107,28 +104,35 @@ def evolve(r_rocket,v_rocket, n_steps, method = 'RK4'):
             z3_dot = v[:,n] + time_step*z2_ddot
 
             z3_ddot = acceleration(z3, time[n]+time_step)
-            # Combine all
+
+            # Combine
             r[:, n+1] = r[:, n] + (1/6)*time_step*(v[:, n] + 2*z1_dot + 2*z2_dot + z3_dot)
             v[:, n+1] = v[:, n] + (1/6)*time_step*(r_ddot + 2*z1_ddot + 2*z2_ddot + z3_ddot)
 
 
     return r, v
 
+initial_earth, initial_moon = pos_earth_moon(0)
+pos_L2 = [initial_moon[0]+d*(m_m/(3*m_e))**(1/3), 0] # m
+v_L2 = [0,-(2*np.pi/T) * pos_L2[0]]
 
-pos_rocket, v_rocket = evolve(r0,v0, n_steps, method='Taylor')
+#r0 = np.array([ 4.672203365e6, 6.571000e6])  # m
+#v0 = np.array([ 10988.378, 12.452740 ]) # m/s
+
+r0 = pos_L2
+v0 = v_L2
+
+pos_rocket, v_rocket = evolve(r0,v0, n_steps,method = 'RK4')
 pos_earth, pos_moon = pos_earth_moon(time)
 
+fig = plt.figure(figsize=(8, 8))  
+ax = plt.axes()  
 
-# Create figure and axes explicitly
-fig = plt.figure(figsize=(8, 8))  # adjustable figure size
-ax = plt.axes()  # creates a single Axes object
-
-# Plot on the specific Axes
 ax.plot(pos_earth[0], pos_earth[1], label='Earth Orbit')
 ax.plot(pos_moon[0], pos_moon[1], label='Moon Orbit')
 ax.plot(pos_rocket[0], pos_rocket[1], label='Rocket Trajectory')
 
-# Add labels, legend, grid
+
 ax.set_xlabel('X Position (m)')
 ax.set_ylabel('Y Position (m)')
 ax.set_title('Earth and Moon Orbits relative to Barycenter')
@@ -136,7 +140,6 @@ ax.legend()
 ax.grid(True)
 ax.set_xlim(-5e8, 5e8)
 ax.set_ylim(-5e8, 5e8)
-# Equal aspect ratio for orbits
 ax.set_aspect('equal', 'box')
 
 plt.show()
