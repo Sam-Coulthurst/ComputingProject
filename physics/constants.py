78a19d3b2a92ea0fd9e18145a13fb077ed5f8af0
Sam Coulthurst <pccx32@durham.ac.uk>
2025-10-16 22:15:25 +0100

    Updated constants to match required values

--- physics/constants.py
+++ physics/constants.py
@@ -1,4 +1,4 @@
-G = 6.67430e-11 # m^3 kg^-1 s^-2
-m_earth = 5.972e24 # kg
-m_moon = 7.348e22 # kg
-d_earth_moon = 384400000 # m
+G = 6.6726e-11 # m^3 kg^-1 s^-2
+m_earth = 5.9742e24 # kg
+m_moon = 7.35e22 # kg
+d_earth_moon = 3.844e5 # m
