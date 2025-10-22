43d86b8fd380bcf7eac9ae71f48642513198d248
Sam Coulthurst <pccx32@durham.ac.uk>
2025-10-22 11:00:16 +0100

    Added Scipy method to integrator function
    Added the option to solve the ODE using scipys odeint method.
    
    Added a plot in main to compare Taylor, RK4 and odeint

--- integrator.py
+++ integrator.py
@@ -1,5 +1,7 @@
 import numpy as np
 from physics.dynamics import acceleration
+from scipy.integrate import odeint
+
 
 def evolve(r0,v0, time, time_step, method = 'RK4'):
     
@@ -43,5 +45,23 @@ def evolve(r0,v0, time, time_step, method = 'RK4'):
             # Combine
             r[:, n+1] = r[:, n] + (a/6) * (v[:, n] + 2*z1_dot + 2*z2_dot + z3_dot)
             v[:, n+1] = v[:, n] + (a/6) * (r_ddot + 2*z1_ddot + 2*z2_ddot + z3_ddot)
-
+    elif method == 'odeint':
+        print("Using odeint integrator")
+        y0 = np.concatenate((r[:, 0], v[:, 0]))
+        sol = odeint(derivatives, y0, time)
+        r = sol[:, :2].T
+        v = sol[:, 2:].T
     return r, v
+
+
+def derivatives(y, t):
+    # unpack
+    r = y[:2]   # position vector
+    v = y[2:]   # velocity vector
+    
+    # acceleration function (define this elsewhere)
+    a = acceleration(r, t)
+    
+    # derivatives
+    dydt = np.concatenate((v, a))
+    return dydt
