04115f3fc88dc050f72928c3853d4472ebdd63c6
Sam Coulthurst <pccx32@durham.ac.uk>
2025-10-29 19:40:49 +0000

    Update orbits.py
    Added optimal_L2_orbit which will be used to define the error in delta r

--- physics/orbits.py
+++ physics/orbits.py
@@ -1,5 +1,5 @@
 import numpy as np
-from physics.constants import MU
+from physics.constants import MU, L_UNIT
 
 def orbital_period():
     '''
@@ -16,10 +16,28 @@ def compute_L2():
         pos_L2 (m) - (3 x 1 numpy array) - position of L2 in the barycentric frame
         v_L2 (m) - (3 x 1 numpy array) - [vx (m/s), vy (m/s), vz (m/s)] - velocity of L2 in the barycentric frame
     '''
-
-    pos_L2 = np.array([1-MU + (MU/(3*(1-MU)))**(1/3), 0,0]) #m
+    #delta_r = (MU/(3*(1-MU)))**(1/3)
+    delta_r = 64.5201e6 / L_UNIT  # dimensionless
+    pos_L2 = np.array([1-MU + delta_r, 0,0]) #m
     v_L2 = np.array([0, pos_L2[0], 0]) #m/s
     return pos_L2, v_L2
+
+def optimal_L2_orbit(pos_L2, v0_L2, t):
+    '''
+    Calculates an optimal orbit around L2 for a spacecraft to maintain a stable position with minimal fuel consumption.
+    Inputs:
+        pos_L2 (m) - (3 x 1 numpy array) - position of L2 in the barycentric frame
+        v0_L2 (m/s) - (3 x 1 numpy array) - initial velocity of L2 in the barycentric frame
+        '''
+    r = np.linalg.norm(pos_L2)
+    omega = np.linalg.norm(v0_L2) / r
+
+    x_L2 = r * np.cos(omega*t)
+    y_L2 = r * np.sin(omega*t)
+    z_L2 = np.zeros_like(x_L2)
+
+    r_L2 = np.array([x_L2, y_L2, z_L2])
+    return r_L2
 
 def pos_earth_moon(t, circular=True):
     '''
